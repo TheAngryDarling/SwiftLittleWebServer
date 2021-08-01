37fe6fffa8200e70077d9164800adfbf2a10e0c0
Tyler Anger <30189019+TheAngryDarling@users.noreply.github.com>
2021-08-01 19:47:53 -0400

    Created new protocols to define client connection details starting with the base protocol LittleWebServerClientDetails Each Client connection (LittleWebServerClient) will be required to have a property connectionDetails: LittleWebServerClientDetails { get } so that this property can be passed to the serverEvent callback when needed Removed all read/parse methods that are specific to HTTP/1.1 to the HTTPParserV1_1 class

--- Sources/LittleWebServer/protocols/LittleWebServerClient.swift
+++ Sources/LittleWebServer/protocols/LittleWebServerClient.swift
@@ -21,37 +21,128 @@ public enum LittleWebServerClientError: Swift.Error {
     case endOfStreamReached
 }
 
+/// Client Connection Details
+public protocol LittleWebServerClientDetails {
+    /// The unique ID of  the given connection
+    var uid: String { get }
+    /// A Unique Identifier for the Client.
+    var uuid: UUID { get }
+    /// The scheme/protocol used for communication between the server and the client like http, https, unix, or your own unique scheme
+    var scheme: String { get }
+}
 /// Representation of any client connection wether it be Unix File Socket, TCP/IP Socket, or
 /// any other custom connection
 public protocol LittleWebServerClient: LittleWebServerConnection,
                                        LittleWebServerClientReader,
-                                       LittleWebServerClientWriter {}
+                                       LittleWebServerClientWriter {
+    
+    /// The connection details for this connection.  These details weill be used when signaling server events
+    var connectionDetails: LittleWebServerClientDetails { get }
+    
+    /// The unique ID of  the given connection
+    var uid: String { get }
+    /// A Unique Identifier for the Client.
+    var uuid: UUID { get }
+    /// The scheme/protocol used for communication between the server and the client like http, https, unix, or your own unique scheme
+    var scheme: String { get }
+    
+}
+
+public extension LittleWebServerClient {
+    var uid: String { return self.connectionDetails.uid }
+    var uuid: UUID { return self.connectionDetails.uuid }
+    var scheme: String { return self.connectionDetails.scheme }
+}
+
+/// Socket Client Connection Details
+public protocol LittleWebServerSocketClientDetails: LittleWebServerClientDetails {
+    /// The socket address for the client connection
+    var address: LittleWebServerSocketConnection.Address { get }
+}
 
 /// Represents an client connection that uses sockets
 public protocol LittleWebServerSocketClient: LittleWebServerClient {
     /// The socket address for the client connection
     var address: LittleWebServerSocketConnection.Address { get }
+    
+    /// The connection details for this connection.  These details weill be used when signaling server events
+    var socketConnectionDetails: LittleWebServerSocketClientDetails { get }
+    
 }
-/// Represents an client connection that uses TCP/IP Sockets
-public protocol LittleWebServerTCPIPSocketClient: LittleWebServerClient {
+
+public extension LittleWebServerSocketClient {
+    var connectionDetails: LittleWebServerClientDetails {
+        return self.socketConnectionDetails
+    }
+    var address: LittleWebServerSocketConnection.Address {
+        return self.socketConnectionDetails.address
+    }
+}
+/// TCP/IP Socket Client Connection Details
+public protocol LittleWebServerTCPIPSocketClientDetails: LittleWebServerSocketClientDetails {
     /// The TCP/IP Address of the client connection
     var ipAddress: LittleWebServerSocketConnection.Address.IP { get }
     /// The TCP/IP Port assigned to the client connection
     var port: LittleWebServerSocketConnection.Address.TCPIPPort { get }
 }
-
-public extension LittleWebServerTCPIPSocketClient where Self: LittleWebServerSocketClient {
-    var ipAddress: LittleWebServerSocketConnection.Address.IP { return self.address.ipAddress! }
-    var port: LittleWebServerSocketConnection.Address.TCPIPPort { return self.address.tcpPort! }
+public extension LittleWebServerTCPIPSocketClientDetails {
+    var ipAddress: LittleWebServerSocketConnection.Address.IP {
+        return self.address.ipAddress!
+    }
+    var port: LittleWebServerSocketConnection.Address.TCPIPPort {
+        return self.address.tcpPort!
+    }
 }
 
-/// Represents an client connection that uses Unix File Sockets
-public protocol LittleWebServerUnixFileSocketClient: LittleWebServerClient {
+/// Represents an client connection that uses TCP/IP Sockets
+public protocol LittleWebServerTCPIPSocketClient: LittleWebServerSocketClient {
+    /// The TCP/IP Address of the client connection
+    var ipAddress: LittleWebServerSocketConnection.Address.IP { get }
+    /// The TCP/IP Port assigned to the client connection
+    var port: LittleWebServerSocketConnection.Address.TCPIPPort { get }
+    
+    /// The connection details for this connection.  These details weill be used when signaling server events
+    var tcpIPSocketConnectionDetails: LittleWebServerTCPIPSocketClientDetails { get }
+}
+
+public extension LittleWebServerTCPIPSocketClient {
+    var ipAddress: LittleWebServerSocketConnection.Address.IP {
+        return self.tcpIPSocketConnectionDetails.ipAddress
+    }
+    var port: LittleWebServerSocketConnection.Address.TCPIPPort {
+        return self.tcpIPSocketConnectionDetails.port
+    }
+    var socketConnectionDetails: LittleWebServerSocketClientDetails {
+        return self.tcpIPSocketConnectionDetails
+    }
+}
+
+/// TCP/IP Socket Client Connection Details
+public protocol LittleWebServerUnixFileSocketClientDetails: LittleWebServerSocketClientDetails {
     /// The path the the Unix Socket File
     var path: String { get }
 }
-public extension LittleWebServerUnixFileSocketClient where Self: LittleWebServerSocketClient {
+public extension LittleWebServerUnixFileSocketClientDetails {
     var path: String { return self.address.unixPath! }
+    
+    
+}
+
+
+/// Represents an client connection that uses Unix File Sockets
+public protocol LittleWebServerUnixFileSocketClient: LittleWebServerSocketClient {
+    /// The path the the Unix Socket File
+    var path: String { get }
+    
+    /// The connection details for this connection.  These details weill be used when signaling server events
+    var unixFileSocketConnectionDetails: LittleWebServerUnixFileSocketClientDetails { get }
+}
+public extension LittleWebServerUnixFileSocketClient {
+    var path: String { return self.address.unixPath! }
+    var socketConnectionDetails: LittleWebServerSocketClientDetails {
+        return self.unixFileSocketConnectionDetails
+    }
+    
 }
 
 
@@ -59,71 +150,6 @@ enum LittleWebServerClientHTTPReadError: Swift.Error {
     case invalidRead(expectedBufferSize: Int, actualBufferSize: Int)
     case unableToCreateString(from: Data)
     case invalidRequestHead(String)
-}
-
-internal extension LittleWebServerClient {
-    /// Reads one HTTP line from the connection.
-    /// This means that it will keep reading 1 byte at
-    /// a time until it reaches the buffer has a suffix of \r\n
-    func readHTTPLine() throws -> String {
-        return try autoreleasepool {
-            var httpLineData = Data()
-            var readByte: UInt8 = 0
-            repeat {
-                
-                guard (try self.readByte(into: &readByte)) else {
-                    throw LittleWebServerClientHTTPReadError.invalidRead(expectedBufferSize: 1, actualBufferSize: 0)
-                }
-                //print(readByte)
-                httpLineData.append(readByte)
-                
-            } while !httpLineData.hasSuffix(LittleWebServer.CRLF_DATA) && self.isConnected
-            
-            // Remove CR+LF from end of bytes
-            httpLineData.removeLast(2)
-            
-            // Try creating string
-            guard let rtn = /*String(data: httpLineData, encoding: .unicode) ??*/ String(data: httpLineData, encoding: .utf8) else {
-                throw LittleWebServerClientHTTPReadError.unableToCreateString(from: httpLineData)
-            }
-            
-            return rtn
-        }
-    }
-    /// Reads the first line of the HTTP request.
-    /// This includes the method, context path and the HTTP version used
-    func readRequestHead() throws -> LittleWebServer.HTTP.Request.Head {
-        let httpHeadLine = try self.readHTTPLine()
-        //print("Request Head '\(httpHeadLine)'")
-        guard let rtn = LittleWebServer.HTTP.Request.Head.parse(httpHeadLine) else {
-            throw LittleWebServerClientHTTPReadError.invalidRequestHead(httpHeadLine)
-        }
-        return rtn
-    }
-    /// Reads the HTTP Headers that come after the HTTP head
-    /// Each HTTP Line it reads should be {Header Name}: {Header Value}\r\n
-    /// A trailing \r\n is expected to indicate that the headers are finished
-    func readRequestHeaders() throws -> LittleWebServer.HTTP.Request.Headers {
-        var rtn = LittleWebServer.HTTP.Request.Headers()
-        
-        // Read the first header or empty string meaning no headers
-        var workingLine: String = try self.readHTTPLine()
-        while !workingLine.isEmpty {
-            
-            if let r = workingLine.range(of: ": ") {
-                let key = String(workingLine[..<r.lowerBound])
-                let val = String(workingLine[r.upperBound...])
-                rtn[.init(properString: key)] = val
-            } else {
-                rtn[.init(properString: workingLine)] = ""
-            }
-            
-            workingLine = try self.readHTTPLine()
-            
-        }
-        
-        return rtn
-    }
 }
 
 
