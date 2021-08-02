e74ddfdbbb374a45f9e8c8bce5014119e911b0fe
Tyler Anger <30189019+TheAngryDarling@users.noreply.github.com>
2021-08-01 20:11:47 -0400

    Changed LittleWebServer.HTTP.Request.parse method to now use the HTTPParserV1_1 struct

--- Tests/LittleWebServerTests/extensions/Request.swift
+++ Tests/LittleWebServerTests/extensions/Request.swift
@@ -8,33 +8,15 @@
 import Foundation
 @testable import LittleWebServer
 internal extension LittleWebServer.HTTP.Request {
-    /// For Testing Purposes
-    static func parse(from client: LittleWebServerClient,
-                      bodyStream: _LittleWebServerInputStream,
-                      uploadedFiles: inout  [LittleWebServer.HTTP.Request.UploadedFileReference],
-                      tempLocation: URL = URL(fileURLWithPath: NSTemporaryDirectory())) throws -> LittleWebServer.HTTP.Request {
-        let head = try client.readRequestHead()
-        
-        let headers = try client.readRequestHeaders()
-        
-        bodyStream.chunked = headers.transferEncodings.contains(.chunked)
-        
-        //let tempSessionManager = LittleWebServer.DefaultSessionManager()
-        
-        return try LittleWebServer.HTTP.Request.parse(scheme: "http",
-                                                      head: head,
-                                                      headers: headers,
-                                                      bodyStream: bodyStream,
-                                                      uploadedFiles: &uploadedFiles,
-                                                      tempLocation: tempLocation)
-    }
     
     static func parse(from client: LittleWebServerClient,
                       uploadedFiles: inout  [LittleWebServer.HTTP.Request.UploadedFileReference],
                       tempLocation: URL = URL(fileURLWithPath: NSTemporaryDirectory())) throws -> LittleWebServer.HTTP.Request {
-        return try self.parse(from: client,
-                              bodyStream: _LittleWebServerInputStream(client: client),
-                              uploadedFiles: &uploadedFiles,
-                              tempLocation: tempLocation)
+        
+        return try HTTPParserV1_1.parseRequest(scheme: "http",
+                                               uploadedFiles: &uploadedFiles,
+                                               tempLocation: tempLocation,
+                                               from: client)
     }
+ 
 }
